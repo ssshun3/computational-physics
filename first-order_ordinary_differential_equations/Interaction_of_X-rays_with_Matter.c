89f628847551e3493d5c3b7dc5cbd515f58ee27e
ssshun3 <s.kudo.22.7.584@4nonome.com>
2023-09-26 12:52:09 +0900

    試作品1号

--- first-order_ordinary_differential_equations/Interaction_of_X-rays_with_Matter.c
+++ first-order_ordinary_differential_equations/Interaction_of_X-rays_with_Matter.c
@@ -1,34 +1,55 @@
-// x線と物質の相互作用
 #include <stdio.h>
+
 // 微分方程式の関数dydxの定義
-double dydx(double x,double y){
-    // 必要な変数の宣言
-     return //文
+double dydx(double x, double y, double mu){
+    return -mu * y;
 }
 
 // ルンゲクッタ法を計算する関数
-double RK(double x0,double y0,double xf,int n){
-    /*x0: xの初期値、y0: yの初期値、xf: xの終点での値、n: ステップ数*/
-    int i;
-    double h,x,y;
-    double k1,k2,k3,k4;
-    h=(xf-x0)/n;
-    x=x0;
-    y=y0;
-    for (i=0;i<n;i++){
-        k1=dydx(x,y);
-        k2=dydx(x+h/2.0,y+h*k1/2.0);
-        k3=dydx(x+h/2.0,y+h*k2/2.0);
-        k4=dydx(x+h,y+h*k3);
-        y=y+h*(k1+2.0*k2+2.0*k3+k4)/6.0;
-        x=x+h;
- /*必要に応じてforループ内で計算結果をprintf文で出力すること*/
+void calculateIntensity(double mu_m, double rho, char* material, double x0, double y0, double xf, int n){
+    double mu = mu_m * rho;
+    double x, y, h;
+    double k1, k2, k3, k4;
+
+    char filename[100];
+    sprintf(filename, "datafile_%s.txt", material);
+    FILE *outputFile = fopen(filename, "w");
+
+    h = (xf - x0) / n;
+    x = x0;
+    y = y0;
+
+    for (int i = 0; i <= n; i++){
+        fprintf(outputFile, "%lf\t%lf\n", x, y);
+        k1 = dydx(x, y, mu);
+        k2 = dydx(x + h/2.0, y + h*k1/2.0, mu);
+        k3 = dydx(x + h/2.0, y + h*k2/2.0, mu);
+        k4 = dydx(x + h, y + h*k3, mu);
+        y = y + h * (k1 + 2.0*k2 + 2.0*k3 + k4) / 6.0;
+        x = x + h;
     }
-    return(y);
+
+    fclose(outputFile);
 }
+
 int main(void){
-    // 必要な変数の宣言
-    yf=RK(引数); //ルンゲクッタ法を計算する関数の呼び出し
-    /*必要に応じて計算結果をprintf文で出力すること*/
-    return(0);
+    double x0 = 0.0, y0 = 1.0, xf = 5.0; // xf: 深さの最大値
+    int n = 100; // ステップ数
+    double rho_Al = 2.70, rho_Pb = 11.35; // 密度
+
+    // 質量吸収係数（波長0.1nmの場合）
+    double mu_m_Al_0_1 = 13.8;
+    double mu_m_Pb_0_1 = 74.2;
+
+    // 質量吸収係数（波長0.03nmの場合）
+    double mu_m_Al_0_03 = 0.552;
+    double mu_m_Pb_0_03 = 12.8;
+
+    calculateIntensity(mu_m_Al_0_1, rho_Al, "Al_0_1", x0, y0, xf, n);
+    calculateIntensity(mu_m_Pb_0_1, rho_Pb, "Pb_0_1", x0, y0, xf, n);
+
+    calculateIntensity(mu_m_Al_0_03, rho_Al, "Al_0_03", x0, y0, xf, n);
+    calculateIntensity(mu_m_Pb_0_03, rho_Pb, "Pb_0_03", x0, y0, xf, n);
+
+    return 0;
 }
